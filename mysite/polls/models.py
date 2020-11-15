1396f830ba5d34287a0842cd23eb6a27d6102268
Tom Stewart <tomjsstewart@gmail.com>
2020-11-15 15:19:07 +0000

    Create was_published_recently function

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -8,6 +10,10 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 
 class Choice(models.Model):
