e27a7fe0804b9ec4a526253ca04de75542d6c667
Tom Stewart <tomjsstewart@gmail.com>
2020-11-15 15:05:43 +0000

    Create database model configure django to use model and create database

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date_published') # Date published is verbose name
+
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
