9610d3cbfef7db965072d9aa4f74d01326f624c2
Tom Stewart <tomjsstewart@gmail.com>
2020-11-22 23:54:51 +0000

    Add voting via a POST request that updates the database

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Tutorial from: https://docs.djangoproject.com/en/3.1/intro/tutorial01/
+
+# Running instructions
+`python manage.py runserver`
+
+## Creating a new app
+`python manage.py startapp <app>`
+
+## Migrate database
+`python manage.py migrate`
+
+## Django shell
+`python manage.py shell`
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from django.db.models import F
+
+from .models import Question, Choice
 
 
 
@@ -21,8 +24,34 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    contxt = {'question': question}
+    return render(request, 'polls/results.html', context=contxt)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        print(request.POST)
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form
+        context = {'question': question, 'error_message': "You didn't select a choice"}
+        return render(request, 'polls/detail.html', context=context)
+    else:
+        # This generates SQL to run on the Db and python never loads the value of votes
+        # thus we avoid any possibility of a race condition occuring with 2 threads reading 
+        # the same value and both incrementing and saving, so that only one threads work is
+        # saved.
+        selected_choice.votes = F('votes') +1
+        # At this point the db actually updates the value
+        selected_choice.save()
+        # should we wish to actually update the value stored in selected_choice.votes we
+        # must run
+        # Additionally this F persists after the save, so running save() twice would increment 
+        # the value twice
+        selected_choice.refresh_from_db()
+
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
